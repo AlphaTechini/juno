5bc4c7554be6d91c10e672d75283787904db8ee6
thiagodeev <thiagodeev@gmail.com>
2026-03-03 22:24:59 -0300

    refactor(rpc): copy OrderedEvent type from rpcv6

--- rpc/v9/trace_test.go
+++ rpc/v9/trace_test.go
@@ -17,7 +17,6 @@ import (
 	"github.com/NethermindEth/juno/db/memory"
 	"github.com/NethermindEth/juno/mocks"
 	"github.com/NethermindEth/juno/rpc/rpccore"
-	rpcv6 "github.com/NethermindEth/juno/rpc/v6"
 	rpc "github.com/NethermindEth/juno/rpc/v9"
 	"github.com/NethermindEth/juno/starknet"
 	adaptfeeder "github.com/NethermindEth/juno/starknetdata/feeder"
@@ -746,8 +745,8 @@ func TestTraceTransaction(t *testing.T) {
 				CallType:       "CALL",
 				Result:         []felt.Felt{*felt.NewUnsafeFromString[felt.Felt]("0x56414c4944")},
 				Calls:          []rpc.FunctionInvocation{},
-				Events:         []rpcv6.OrderedEvent{},
-				Messages:       []rpcv6.OrderedL2toL1Message{},
+				Events:         []rpc.OrderedEvent{},
+				Messages:       []rpc.OrderedL2toL1Message{},
 				ExecutionResources: &rpc.InnerExecutionResources{
 					L1Gas: 0,
 					L2Gas: 0,
@@ -781,7 +780,7 @@ func TestTraceTransaction(t *testing.T) {
 						CallType:       "DELEGATE",
 						Result:         []felt.Felt{*felt.NewUnsafeFromString[felt.Felt]("0x1")},
 						Calls:          []rpc.FunctionInvocation{},
-						Events: []rpcv6.OrderedEvent{
+						Events: []rpc.OrderedEvent{
 							{
 								Order: 0,
 								Keys:  []*felt.Felt{felt.NewUnsafeFromString[felt.Felt]("0x99cd8bde557814842a3121e8ddfd433a539b8c9f14bf31ebf108d12e6196e9")},
@@ -793,15 +792,15 @@ func TestTraceTransaction(t *testing.T) {
 								},
 							},
 						},
-						Messages: []rpcv6.OrderedL2toL1Message{},
+						Messages: []rpc.OrderedL2toL1Message{},
 						ExecutionResources: &rpc.InnerExecutionResources{
 							L1Gas: 0,
 							L2Gas: 0,
 						},
 					},
 				},
-				Events:   []rpcv6.OrderedEvent{},
-				Messages: []rpcv6.OrderedL2toL1Message{},
+				Events:   []rpc.OrderedEvent{},
+				Messages: []rpc.OrderedL2toL1Message{},
 				ExecutionResources: &rpc.InnerExecutionResources{
 					L1Gas: 0,
 					L2Gas: 0,
@@ -1070,8 +1069,8 @@ func TestAdaptVMTransactionTrace(t *testing.T) {
 			Type: rpc.TxnInvoke,
 			ValidateInvocation: &rpc.FunctionInvocation{
 				Calls:  []rpc.FunctionInvocation{},
-				Events: []rpcv6.OrderedEvent{},
-				Messages: []rpcv6.OrderedL2toL1Message{
+				Events: []rpc.OrderedEvent{},
+				Messages: []rpc.OrderedL2toL1Message{
 					{
 						Order: 0,
 						From:  fromAddr,
@@ -1095,16 +1094,16 @@ func TestAdaptVMTransactionTrace(t *testing.T) {
 			},
 			FeeTransferInvocation: &rpc.FunctionInvocation{
 				Calls:      []rpc.FunctionInvocation{},
-				Events:     []rpcv6.OrderedEvent{},
-				Messages:   []rpcv6.OrderedL2toL1Message{},
+				Events:     []rpc.OrderedEvent{},
+				Messages:   []rpc.OrderedL2toL1Message{},
 				IsReverted: false,
 			},
 			ExecuteInvocation: &rpc.ExecuteInvocation{
 				RevertReason: "",
 				FunctionInvocation: &rpc.FunctionInvocation{
 					Calls:      []rpc.FunctionInvocation{},
-					Events:     []rpcv6.OrderedEvent{},
-					Messages:   []rpcv6.OrderedL2toL1Message{},
+					Events:     []rpc.OrderedEvent{},
+					Messages:   []rpc.OrderedL2toL1Message{},
 					IsReverted: false,
 				},
 			},
@@ -1170,18 +1169,18 @@ func TestAdaptVMTransactionTrace(t *testing.T) {
 			Type: rpc.TxnDeployAccount,
 			ValidateInvocation: &rpc.FunctionInvocation{
 				Calls:    []rpc.FunctionInvocation{},
-				Events:   []rpcv6.OrderedEvent{},
-				Messages: []rpcv6.OrderedL2toL1Message{},
+				Events:   []rpc.OrderedEvent{},
+				Messages: []rpc.OrderedL2toL1Message{},
 			},
 			FeeTransferInvocation: &rpc.FunctionInvocation{
 				Calls:    []rpc.FunctionInvocation{},
-				Events:   []rpcv6.OrderedEvent{},
-				Messages: []rpcv6.OrderedL2toL1Message{},
+				Events:   []rpc.OrderedEvent{},
+				Messages: []rpc.OrderedL2toL1Message{},
 			},
 			ConstructorInvocation: &rpc.FunctionInvocation{
 				Calls:    []rpc.FunctionInvocation{},
-				Events:   []rpcv6.OrderedEvent{},
-				Messages: []rpcv6.OrderedL2toL1Message{},
+				Events:   []rpc.OrderedEvent{},
+				Messages: []rpc.OrderedL2toL1Message{},
 			},
 		}
 
@@ -1211,8 +1210,8 @@ func TestAdaptVMTransactionTrace(t *testing.T) {
 				RevertReason: "",
 				FunctionInvocation: &rpc.FunctionInvocation{
 					Calls:      []rpc.FunctionInvocation{},
-					Events:     []rpcv6.OrderedEvent{},
-					Messages:   []rpcv6.OrderedL2toL1Message{},
+					Events:     []rpc.OrderedEvent{},
+					Messages:   []rpc.OrderedL2toL1Message{},
 					IsReverted: false,
 				},
 			},
@@ -1287,12 +1286,12 @@ func TestAdaptFeederBlockTrace(t *testing.T) {
 						RevertReason: "",
 						FunctionInvocation: &rpc.FunctionInvocation{
 							Calls: []rpc.FunctionInvocation{},
-							Events: []rpcv6.OrderedEvent{{
+							Events: []rpc.OrderedEvent{{
 								Order: 1,
 								Keys:  []*felt.Felt{felt.NewFromUint64[felt.Felt](2)},
 								Data:  []*felt.Felt{felt.NewFromUint64[felt.Felt](3)},
 							}},
-							Messages: []rpcv6.OrderedL2toL1Message{},
+							Messages: []rpc.OrderedL2toL1Message{},
 							ExecutionResources: &rpc.InnerExecutionResources{
 								L1Gas: 10,
 								L2Gas: 11,
@@ -1335,14 +1334,14 @@ func TestAdaptFeederBlockTrace(t *testing.T) {
 					Type: rpc.TxnInvoke,
 					FeeTransferInvocation: &rpc.FunctionInvocation{
 						Calls:              []rpc.FunctionInvocation{},
-						Events:             []rpcv6.OrderedEvent{},
-						Messages:           []rpcv6.OrderedL2toL1Message{},
+						Events:             []rpc.OrderedEvent{},
+						Messages:           []rpc.OrderedL2toL1Message{},
 						ExecutionResources: &rpc.InnerExecutionResources{},
 					},
 					ValidateInvocation: &rpc.FunctionInvocation{
 						Calls:              []rpc.FunctionInvocation{},
-						Events:             []rpcv6.OrderedEvent{},
-						Messages:           []rpcv6.OrderedL2toL1Message{},
+						Events:             []rpc.OrderedEvent{},
+						Messages:           []rpc.OrderedL2toL1Message{},
 						ExecutionResources: &rpc.InnerExecutionResources{},
 					},
 					ExecuteInvocation: &rpc.ExecuteInvocation{
